ad78aeeb954412226252c448dba0a43ea5a578cb
Your Name <you@example.com>
2021-08-13 19:24:29 +0300

    add progress navigation

---
+++ function.js
@@ -0,0 +1,10 @@
+export default function uppercaseString (text) {
+  return text.toUpperCase();
+}
+const lowercaseString = (text) => {
+  return text.toLowerCase();
+}
+// console.log(uppercaseString('hello'));
+// console.log(lowercaseString('HELLO'));
+
+// export {uppercaseString, lowercaseString};
---
+++ index.js
@@ -0,0 +1,2 @@
+import add from "./function.js";
+console.log(add('hello'));
